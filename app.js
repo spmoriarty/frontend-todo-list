1c5a0f913cba3cbedb65d2a59f245d6964d6f71e
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-23 16:06:36 -0700

    building out front end form and fetches

---
+++ app.js
@@ -0,0 +1,17 @@
+import { redirectLogin, signUp } from './lib/services/fetch-utils';
+
+const signUpForm = document.getElementById('signUp');
+
+signUpForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const userForm = new userForm(signUpForm);
+  await signUp({
+    email: userForm.get('email'),
+    password: userForm.get('password'),
+    firstName: userForm.get('first-name'), //USER TABLES?
+    lastName: userForm.get('last-name')
+  });
+});
+
+
+redirectLogin();
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./lib/app');
+const app = require('./app.js');
 const pool = require('./lib/utils/pool');
 
 const API_URL = process.env.API_URL || 'http://localhost';
